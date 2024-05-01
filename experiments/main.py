ffa63886e3cf56f3cba94c6a34823e0880cb368f
Philip Hierhager <philip.hierhager@googlemail.com>
2024-05-01 16:32:48 +0200

    small changes to template config

--- experiments/main.py
+++ experiments/main.py
@@ -2,9 +2,7 @@ from runner import Runner
 from experiment_generation import create_experiment_config
 
 if __name__ == "__main__":
-    file_path = (
-        r"experiments\easy.yaml"  # Provide the path to your YAML configuration file
-    )
+    file_path = r"experiments\configs\template.yaml"  # Provide the path to your YAML configuration file
     experiment_config = create_experiment_config(file_path)
     runner = Runner(experiment_config)
     experiment_result = runner.run_experiment()
